00d8c96d40ad4bd30ed96ff77711611c91fb4724
Tommy Flynn <TFly.Flynn@gmail.com>
2019-10-22 10:16:27 -0400

    adding some small changes with functions.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -32,7 +32,7 @@ $(document).ready(function () {
   // I'm gonna start with some variables. 
 
   // This is going to be my random number. I just need to make sure it doesn't reset when until a win or loss function happens. 
-  var computerGuess = Math.floor(Math.random() * 120) +1;
+  var computerGuess = Math.floor(Math.random() * 120) + 1;
   // Let's console log that to make sure the random number is being generated. 
   console.log(computerGuess);
   // Some variables. 
@@ -46,11 +46,11 @@ $(document).ready(function () {
   var button4 = 0;
   // Now we want to assign the random value to the buttons. But they need to be 1-12. 
   function newGame() {
-     button1 = Math.floor(Math.random() * 12) + 1;
-     button2 = Math.floor(Math.random() * 12) + 1;
-     button3 = Math.floor(Math.random() * 12) + 1;
-     button4 = Math.floor(Math.random() * 12) + 1;
-     computerGuess = 1 + Math.floor(Math.random() * 120);
+    button1 = Math.floor(Math.random() * 12) + 1;
+    button2 = Math.floor(Math.random() * 12) + 1;
+    button3 = Math.floor(Math.random() * 12) + 1;
+    button4 = Math.floor(Math.random() * 12) + 1;
+    computerGuess = 1 + Math.floor(Math.random() * 120);
     // Let's console log that to make sure we're getting an assignment. 
     console.log(button1);
     console.log(button2);
@@ -60,29 +60,53 @@ $(document).ready(function () {
     console.log(computerGuess);
   };
 
+  // Now we need to make a win and loss variable function. 
+  function winLoss() {
+    if (userScore === computerGuess) {
+      wins++;
+      console.log(wins);
+    }
+    if (userScore > computerGuess) {
+      losses++;
+      console.log(losses);
+    }
+  }
+  // We need a reset function. A function that will automatically reset the userscore and computerguess. 
+  function reset () {
+    userScore = 0;
+    computerGuess = 0;
+    newGame();
+  };
+
+// Our newGame function will run each time the game is reset. It will include randomized values for all four buttons. 
   newGame();
+
+  computerGuess = Math.floor(Math.random() * 120) + 1;
 
   $("#button-1").on("click", function () {
     userScore = button1 + userScore;
     console.log(userScore);
-      $("#Score").text( "Result: " + userScore);
+    $("#Score").text("Result: " + userScore);
   });
   $("#button-2").on("click", function () {
     userScore = button2 + userScore;
     console.log(userScore);
-      $("#Score").text("Result: " + userScore);
+    $("#Score").text("Result: " + userScore);
   });
   $("#button-3").on("click", function () {
     userScore = button3 + userScore;
     console.log(userScore);
-     $("#Score").text("Result: " + userScore);
+    $("#Score").text("Result: " + userScore);
   });
   $("#button-4").on("click", function () {
     userScore = button4 + userScore;
     console.log(userScore);
-      $("#Score").text("Result: " + userScore);
+    $("#Score").text("Result: " + userScore);
   });
-  
+
+  winLoss();
+  reset();
+
 });
 
 // Let's make the buttons and make sure we are getting a random value for each. 
